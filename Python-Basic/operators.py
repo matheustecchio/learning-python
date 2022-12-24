acb61d889f62d715db29f7efcc8bc193422be80c
matheustecchio <matheusdiastecchio@gmail.com>
2022-12-24 16:25:39 -0300

    Upload: arithmetic operators

--- Python-Basic/operators.py
+++ Python-Basic/operators.py
@@ -4,13 +4,22 @@
 x = 20
 y = 10
 
-x +  y   # Addition
-x -  y   # Subtraction
-x *  y   # Multiplication
-x /  y   # Division
-x %  y   # Modulus
-x ** y  # Exponentiation
-x // y  # Floor division
+o1 = x +  y   # Addition
+o2 = x -  y   # Subtraction
+o3 = x *  y   # Multiplication
+o4 = x /  y   # Division
+o5 = x %  y   # Modulus
+o6 = x ** y  # Exponentiation
+o7 = x // y  # Floor division
+
+print(o1)
+print(o2)
+print(o3)
+print(o3)
+print(o4)
+print(o5)
+print(o6)
+print(o7)
 
 #%% 
 # Boolean examples
