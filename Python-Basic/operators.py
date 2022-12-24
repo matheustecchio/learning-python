75cbada064a76dc7ba4055e9084916cc568f309a
matheustecchio <matheusdiastecchio@gmail.com>
2022-12-24 16:22:14 -0300

    Add: arithmetic operators

---
+++ Python-Basic/operators.py
@@ -0,0 +1,21 @@
+#%%
+# Arithmetic Operators
+
+x = 20
+y = 10
+
+x +  y   # Addition
+x -  y   # Subtraction
+x *  y   # Multiplication
+x /  y   # Division
+x %  y   # Modulus
+x ** y  # Exponentiation
+x // y  # Floor division
+
+#%% 
+# Boolean examples
+
+print(1 < 2)
+print(1 < 1)
+print(1 == 1)
+print(1 >= 1)
