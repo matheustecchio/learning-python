3d9a4bb0aae048a4bf72f1cc28c75504d706a1de
matheustecchio <matheusdiastecchio@gmail.com>
2022-12-24 16:30:59 -0300

    Add: comparison operators

--- Python-Basic/operators.py
+++ Python-Basic/operators.py
@@ -21,6 +21,19 @@ print(o5)
 print(o6)
 print(o7)
 
+#%%
+# Comparison Operators
+
+x = 20
+y = 10
+
+print(x == y)   # Equal
+print(x != y)   # Not equal
+print(x < y)    # Greater than
+print(x > y)    # Less than
+print(x <= y)   # Greater than or equal to
+print(x >= y)   # Less than or equal to
+
 #%% 
 # Boolean examples
 
